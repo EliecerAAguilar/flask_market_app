450c67d9d67ed5dbac13f0a94e62753905b7bd6f
Eliecer Aguilar <eaguilar@ayudinga.org>
2022-08-13 21:07:30 -0500

    database sqlite3

--- app.py
+++ app.py
@@ -31,11 +31,7 @@ def home():
 
 @app.route('/market')
 def market():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Item.query.all()
     return render_template('market.html', items=items)
 
 
