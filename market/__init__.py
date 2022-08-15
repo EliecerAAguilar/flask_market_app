f933b828232cde182da41873a20ecf5ae9878ff7
Eliecer Aguilar <eaguilar@ayudinga.org>
2022-08-15 15:34:32 -0500

    new project struture

--- market/__init__.py
+++ market/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/market.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-from market import routes # IDK why but DO NOT MOVE THIS LINE
+from market.routes import main# IDK why but DO NOT MOVE THIS LINE
--- market/routes/main.py
+++ market/routes/main.py
@@ -1,6 +1,6 @@
 from market import app
 from flask import render_template
-from market.models import Item
+from market.models.models import Item
 
 
 @app.route('/')
